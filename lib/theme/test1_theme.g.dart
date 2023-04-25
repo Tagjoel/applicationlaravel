4cd0f42d44037dd619c5eedc9cf5aad08fc3c8db
Parabeac-Cloud <admin@parabeac.com>
2023-04-25 14:10:45 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/theme/test1_theme.g.dart
@@ -0,0 +1,19 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';import 'package:test1/theme/test1_colors.g.dart';
+
+class Test1Theme {
+
+static const ColorScheme light = ColorScheme.light(onPrimary: Test1Colors.lightOnPrimary,primary: Test1Colors.lightPrimary,primaryContainer: Test1Colors.lightPrimaryContainer,secondary: Test1Colors.lightSecondary,onSecondary: Test1Colors.lightOnSecondary,onPrimaryContainer: Test1Colors.lightOnPrimaryContainer,secondaryContainer: Test1Colors.lightSecondaryContainer,onSecondaryContainer: Test1Colors.lightOnSecondaryContainer,shadow: Test1Colors.lightShadow);
+
+static  ThemeData themeDataLight = ThemeData( colorScheme: light,);
+
+static const ColorScheme dark = ColorScheme.dark(primary: Test1Colors.darkPrimary,onPrimary: Test1Colors.darkOnPrimary,primaryContainer: Test1Colors.darkPrimaryContainer,onPrimaryContainer: Test1Colors.darkOnPrimaryContainer,secondary: Test1Colors.darkSecondary,onSecondary: Test1Colors.darkOnSecondary,secondaryContainer: Test1Colors.darkSecondaryContainer,onSecondaryContainer: Test1Colors.darkOnSecondaryContainer,shadow: Test1Colors.darkShadow);
+
+static  ThemeData themeDataDark = ThemeData( colorScheme: dark,);
+}
